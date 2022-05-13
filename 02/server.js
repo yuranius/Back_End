3f0d6f4b4ca15b7c52baf7a8e6017158b1ad9681
yuranius <yuranius.webber@gmail.com>
2022-05-13 17:28:20 +0300

    start

--- 02/server.js
+++ 02/server.js
@@ -1,1 +1,12 @@
-console.log('привет')
+const http = require('http');
+
+let requestsCount = 0;
+
+const server = http.createServer((request, response) => {
+
+    requestsCount ++
+    response.write('yuranius: '+ requestsCount)
+    response.end()
+});
+
+server.listen(3003)
