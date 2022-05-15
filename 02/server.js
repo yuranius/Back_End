a1f6b74ff80822be497f95cfea8c654b81f68d5d
yuranius <yuranius.webber@gmail.com>
2022-05-15 08:33:44 +0300

    over

--- 02/server.js
+++ 02/server.js
@@ -3,10 +3,12 @@ const http = require('http');
 let requestsCount = 0;
 
 const server = http.createServer((request, response) => {
-
+    
     requestsCount ++
     response.write('yuranius: '+ requestsCount)
     response.end()
 });
+
+console.log('сервер запущен на порту 3003');
 
 server.listen(3003)
