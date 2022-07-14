48867cea906935fe728ed1764c60d60aee513b28
yuranius <yuranius.webber@gmail.com>
2022-07-14 21:26:46 +0300

    05

---
+++ 05_rest_api/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+
+res.send({message: "Hello!!!"})
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
