afe6543b3142252b7b1181dfd53d4e2446426fa2
yuranius <yuranius.webber@gmail.com>
2022-07-16 22:12:15 +0300

    kirsanov

--- 05_rest_api/dist/index.js
+++ 05_rest_api/dist/index.js
@@ -7,8 +7,33 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send({ message: "Hello!!!" });
+    res.sendStatus(404);
+});
+app.get('/courses', (req, res) => {
+    res.json([
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
+        { id: 3, title: "yuranius" },
+        { id: 4, title: "css" },
+        { id: 5, title: "reactjs" }
+    ]);
+});
+app.get('/courses/:id', (req, res) => {
+    const foundCourses = [
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
+        { id: 3, title: "yuranius" },
+        { id: 4, title: "css" },
+        { id: 5, title: "reactjs" }
+    ].find(c => c.id === +req.params.id);
+    res.json(foundCourses);
+    if (!foundCourses) {
+        res.sendStatus(404);
+        return;
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+// запрос 
+// fetch('http://localhost:3000', {method:'GET'}).then(res => res.json()).then(json => console.log(json)) 
--- 05_rest_api/src/index.ts
+++ 05_rest_api/src/index.ts
@@ -2,9 +2,43 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
 
-res.send({message: "Hello!!!"})
+
+
+app.get('/', (req,res) => {
+  res.sendStatus(404)
+})
+
+app.get('/courses', (req, res) => {
+  res.json ([
+    {id:1, title:"front-end"},
+    {id:2, title:"back-end"},
+    {id:3, title:"yuranius"},
+    {id:4, title:"css"},
+    {id:5, title:"reactjs"}
+  ])
+})
+
+app.get('/courses/:id', (req, res) => {
+  const foundCourses = [
+    { id: 1, title: "front-end" },
+    { id: 2, title: "back-end" },
+    { id: 3, title: "yuranius" },
+    { id: 4, title: "css" },
+    { id: 5, title: "reactjs" }
+  ].find(c => c.id === +req.params.id)
+
+
+
+  res.json (foundCourses)
+ 
+
+  if(!foundCourses){
+    res.sendStatus(404)
+    return
+  }
+
+
 })
 
 
@@ -12,3 +46,7 @@ res.send({message: "Hello!!!"})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// запрос 
+// fetch('http://localhost:3000', {method:'GET'}).then(res => res.json()).then(json => console.log(json)) 
